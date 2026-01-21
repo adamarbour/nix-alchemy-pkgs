160854f776cc9a5798f375733f9cd995089458b8
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-01-21 10:17:50 -0600

    Fixing overlay

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,6 @@
 final: prev:
 let
-  lib = prev.lib;
+  lib = final.lib;
   pkgDirs = lib.filterAttrs (_name: type: type == "directory")
     (builtins.readDir ./pkgs);
   myPkgs = lib.mapAttrs (name: _type: final.callPackage (./pkgs + "/${name}/package.nix") { }) pkgDirs;
