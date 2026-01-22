9b0411c1d12dabfcfdcd4029c2c90f7125c96aa7
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-01-21 19:32:42 -0600

    Including sources in overlay

--- overlay.nix
+++ overlay.nix
@@ -1,9 +1,10 @@
 final: prev:
 let
+  sources = ./npins;
   lib = final.lib;
   pkgDirs = lib.filterAttrs (_name: type: type == "directory")
     (builtins.readDir ./pkgs);
-  myPkgs = lib.mapAttrs (name: _type: final.callPackage (./pkgs + "/${name}/package.nix") { }) pkgDirs;
+  myPkgs = lib.mapAttrs (name: _type: final.callPackage (./pkgs + "/${name}/package.nix") { inherit sources; }) pkgDirs;
 in {
   mypkgs = myPkgs;
 }
